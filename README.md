8b4925d821d1a1febcedc5fb1b88a523501c1aff
cravesoft <cravesoft@gmail.com>
2016-09-24 17:09:23 +0200

    add option to download resolutions

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
-# un-scraper - a scrapping tool to download General Assembly records from the United Nations
+# un-scraper - a scrapping tool to download documents from the United Nations Website
 
 ## Install
 
 ```
 git clone or download the code
-npm install
+npm install -g casperjs phantomjs
 ```
 
 ## Usage
 
-```
-PATH=$PATH:./node_modules/phantomjs/bin/ node_modules/casperjs/bin/casperjs un_scraper.js [options]
-```
+  Usage: casperjs un-scraper.js [options]
+
+  Options:
+
+    --type        Specify the type of document. Possible values are ``pv`` and
+                  ``res``. The default value is ``pv``.
+    --session-id  The session identifier. The default value is 1.
+    --doc-id      The document identifier. The default value is 1.
+
+  Any other options are passed to casperj (see `casperjs --help`)
+
+  Examples:
+
+    $ casperjs un-scraper.js --type=res --session-id 48 --doc-id 20
+    $ casperjs un-scraper.js --type=pv --session-id 68 --doc-id 192
---
+++ un-scraper.js
@@ -0,0 +1,75 @@
+var casper = require('casper').create({
+  verbose: true,
+  logLevel: 'debug',
+  pageSettings: {
+    webSecurityEnabled: false
+  }
+});
+
+casper.start();
+
+casper.then(function() {
+  var sessionId = 1;
+  var docId = 1;
+  var sep1;
+  var sep2;
+  var urlPrefix = 'http://www.un.org/en/ga/search/view_doc.asp?symbol=A';
+
+  casper.cli.drop("cli");
+  casper.cli.drop("casper-path");
+  if (casper.cli.args.length === 0 &&
+      Object.keys(casper.cli.options).length === 0) {
+    this.echo("No arg nor option passed").exit();
+  }
+
+  if (casper.cli.has('type') && casper.cli.get('type') !== 'pv') {
+    if (casper.cli.get('type') === 'res') {
+      sep1 = '/RES/';
+      sep2 = '/';
+    }
+  } else {
+    sep1 = '/';
+    sep2 = '/PV.';
+  }
+
+  if (casper.cli.has('session-id'))
+    sessionId = casper.cli.get('session-id');
+  if (casper.cli.has('doc-id'))
+    docId = casper.cli.get('doc-id');
+
+  (function(url, sessionId) {
+    casper.thenOpen(url, function() {
+      this.echo('casper.async: ' + url);
+    });
+
+    casper.withFrame('mainFrame', function() {
+      this.echo('casper.getCurrentUrl: ' + this.getCurrentUrl());
+      (function(self, frameUrl, sessionId) {
+        casper.thenOpen(frameUrl, function () {
+          self.echo('open ' + frameUrl);
+        });
+
+        casper.waitFor(function check() {
+          var content = self.getElementAttribute(
+              'meta[http-equiv="refresh"]', 'content');
+          if (content === null)
+            return;
+          var url = content.split('; ')[1].split('=')[1];
+          var filename = './' + sessionId + '/' +
+            url.replace(/^.*[\\\/]/, '').split('?')[0];
+          self.echo('download ' + url);
+          self.echo('to ' + filename);
+          return self.download(url, filename);
+        }, function then() {
+          self.echo('then');
+        }, function timeout() {
+          self.echo('timeout');
+        });
+      })(this, this.getCurrentUrl(), sessionId);
+    });
+  })(urlPrefix + sep1 + sessionId + sep2 + docId, sessionId);
+});
+
+casper.run(function() {
+  this.echo('Done.').exit();
+});
